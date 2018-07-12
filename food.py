146e1a476781538d12399bd0141b025aca4cb6ef
Ron <rohan95.gupta@gmail.com>
2018-07-12 23:32:29 +0530

    Thirsty feature added

--- food.py
+++ food.py
@@ -4,3 +4,8 @@ if hungry.lower() == "yes" or hungry.lower() =="y":
     print("Eat pizza")
     print("Eat maggi")
     print("Eat burger")
+    
+thirsty = raw_input("Are you thirsty?")
+
+if thirsty.lower() == "yes" or thirsty.lower() =="y":
+    print("Drink Milk")
